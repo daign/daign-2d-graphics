3a93633a5d4f7515a86d717c5c75adf6db742d8d
daign <jakob.mischek@daign.de>
2022-03-01 19:58:32 +0100

    added more unit tests

--- test/application/controlPoint.spec.ts
+++ test/application/controlPoint.spec.ts
@@ -1,8 +1,9 @@
 import { expect } from 'chai';
 
 import { Matrix3, Vector2 } from '@daign/math';
+import { View } from '@daign/2d-pipeline';
 
-import { Application, ControlObject, ControlPoint } from '../../lib';
+import { Application, ControlObject, ControlPoint, RoundingModifier } from '../../lib';
 import { TestContext } from '../testContext';
 
 class TestObject extends ControlObject {
@@ -28,6 +29,94 @@ describe( 'ControlPoint', (): void => {
 
       // Assert
       expect( controlPoint.center.equals( new Vector2( 3, 5 ) ) ).to.be.true;
+    } );
+  } );
+
+  describe( 'snap', (): void => {
+    it( 'should save a snapshot of the current center', (): void => {
+      // Arrange
+      const context = new TestContext();
+      const targetPoint = new Vector2( 1, 2 );
+      const targetTransformation = new Matrix3().setTranslation( new Vector2( 2, 3 ) );
+      const application = new Application( context );
+      const controlObject = new TestObject();
+      const controlPoint = new ControlPoint( targetPoint, targetTransformation, application,
+        controlObject, 0 );
+      expect( controlPoint.center.equals( new Vector2( 3, 5 ) ) ).to.be.true;
+
+      // Act
+      controlPoint.snap();
+
+      // Assert
+      expect( controlPoint.center.snapshot!.equals( new Vector2( 3, 5 ) ) ).to.be.true;
+    } );
+  } );
+
+  describe( 'drag', (): void => {
+    it( 'should apply the translation delta to the last snapshot of the center', (): void => {
+      // Arrange
+      const context = new TestContext();
+      const application = new Application( context );
+
+      const view = new View();
+      view.mountNode( application );
+
+      const targetPoint = new Vector2( 1, 2 );
+
+      const controlObject = new TestObject();
+      controlObject.points.push( targetPoint );
+      application.drawingLayer.appendChild( controlObject );
+
+      const targetTransformation = new Matrix3().setIdentity();
+      const controlPoint = new ControlPoint( targetPoint, targetTransformation, application,
+        controlObject, 0 );
+
+      expect( controlPoint.center.equals( targetPoint ) ).to.be.true;
+      controlPoint.snap();
+
+      // Act
+      // First drag does not influence the final position, because snapshot remains the same.
+      controlPoint.drag( new Vector2( 1, 1 ) );
+      controlPoint.drag( new Vector2( 2, 3 ) );
+
+      // Assert
+      expect( controlPoint.center.equals( new Vector2( 3, 5 ) ) ).to.be.true;
+      expect( controlObject.points.getElement( 0 )!.equals( new Vector2( 3, 5 ) ) ).to.be.true;
+    } );
+
+    it( 'should apply a control modifier when the point is dragged', (): void => {
+      // Arrange
+      const context = new TestContext();
+      const application = new Application( context );
+
+      const view = new View();
+      view.mountNode( application );
+
+      const targetPoint = new Vector2( 1, 2 );
+
+      const modifier = new RoundingModifier();
+
+      const controlObject = new TestObject();
+      controlObject.controlModifier = modifier;
+      controlObject.points.push( targetPoint );
+      application.drawingLayer.appendChild( controlObject );
+
+      const targetTransformation = new Matrix3().setIdentity();
+      const controlPoint = new ControlPoint( targetPoint, targetTransformation, application,
+        controlObject, 0 );
+
+      expect( controlPoint.center.equals( targetPoint ) ).to.be.true;
+      controlPoint.snap();
+
+      // Act
+      // First drag does not influence the final position, because snapshot remains the same.
+      controlPoint.drag( new Vector2( 1, 1 ) );
+      controlPoint.drag( new Vector2( 2.1, 3.5 ) );
+
+      // Assert
+      // The control point is not rounded, but the point applied back to the control object is.
+      expect( controlPoint.center.equals( new Vector2( 3.1, 5.5 ) ) ).to.be.true;
+      expect( controlObject.points.getElement( 0 )!.equals( new Vector2( 3, 6 ) ) ).to.be.true;
     } );
   } );
 } );
