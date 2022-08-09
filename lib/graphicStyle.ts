d381e14a60bdb213f9e82a5d7907239ab9d3fdd5
daign <jakob.mischek@daign.de>
2022-08-09 17:16:10 +0200

    added more styling attributes to GraphicStyle

--- lib/graphicStyle.ts
+++ lib/graphicStyle.ts
@@ -4,11 +4,41 @@ import { IStyleDeclaration } from '@daign/style-sheets';
  * Implementation of IStyleDeclaration.
  */
 export class GraphicStyle implements IStyleDeclaration {
+  // Fill.
   public fill: string | null = null;
+  public fillOpacity: number | null = null;
+  public fillRule: string | null = null;
+
+  // Stroke.
   public stroke: string | null = null;
   public strokeWidth: number | null = null;
+  public strokeOpacity: number | null = null;
+  public strokeLinecap: string | null = null;
+  public strokeLinejoin: string | null = null;
+  public strokeMiterlimit: number | null = null;
+  public strokeDasharray: string | null = null;
+  public strokeDashoffset: number | null = null;
+  public vectorEffect: string | null = null;
+
+  // Display.
+  public display: string | null = null;
+  public visibility: string | null = null;
   public opacity: number | null = null;
+
+  // Text.
+  public fontFamily: string | null = null;
   public fontSize: string | null = null;
+  public fontStyle: string | null = null;
+  public fontVariant: string | null = null;
+  public fontWeight: string | null = null;
+  public fontStretch: string | null = null;
+  public letterSpacing: string | null = null;
+  public wordSpacing: string | null = null;
+  public textDecoration: string | null = null;
+
+  // Interactivity.
+  public pointerEvents: string | null = null;
+  public cursor: string | null = null;
 
   /**
    * Returns whether the declaration is empty (all attributes are equal null).
@@ -16,25 +46,99 @@ export class GraphicStyle implements IStyleDeclaration {
   public get isEmpty(): boolean {
     return (
       this.fill === null &&
+      this.fillOpacity === null &&
+      this.fillRule === null &&
       this.stroke === null &&
       this.strokeWidth === null &&
+      this.strokeOpacity === null &&
+      this.strokeLinecap === null &&
+      this.strokeLinejoin === null &&
+      this.strokeMiterlimit === null &&
+      this.strokeDasharray === null &&
+      this.strokeDashoffset === null &&
+      this.vectorEffect === null &&
+      this.display === null &&
+      this.visibility === null &&
       this.opacity === null &&
-      this.fontSize === null
+      this.fontFamily === null &&
+      this.fontSize === null &&
+      this.fontStyle === null &&
+      this.fontVariant === null &&
+      this.fontWeight === null &&
+      this.fontStretch === null &&
+      this.letterSpacing === null &&
+      this.wordSpacing === null &&
+      this.textDecoration === null &&
+      this.pointerEvents === null &&
+      this.cursor === null
     );
   }
 
   /**
    * Constructor.
    * @param fill - Optional.
+   * @param fillOpacity - Optional.
+   * @param fillRule - Optional.
    * @param stroke - Optional.
    * @param strokeWidth - Optional.
+   * @param strokeOpacity - Optional.
+   * @param strokeLinecap - Optional.
+   * @param strokeLinejoin - Optional.
+   * @param strokeMiterlimit - Optional.
+   * @param strokeDasharray - Optional.
+   * @param strokeDashoffset - Optional.
+   * @param vectorEffect - Optional.
+   * @param display - Optional.
+   * @param visibility - Optional.
    * @param opacity - Optional.
+   * @param fontFamily - Optional.
    * @param fontSize - Optional.
+   * @param fontStyle - Optional.
+   * @param fontVariant - Optional.
+   * @param fontWeight - Optional.
+   * @param fontStretch - Optional.
+   * @param letterSpacing - Optional.
+   * @param wordSpacing - Optional.
+   * @param textDecoration - Optional.
+   * @param pointerEvents - Optional.
+   * @param cursor - Optional.
    */
-  public constructor( fill?: string, stroke?: string, strokeWidth?: number,
-    opacity?: number, fontSize?: string ) {
+  public constructor(
+    fill?: string,
+    fillOpacity?: number,
+    fillRule?: string,
+    stroke?: string,
+    strokeWidth?: number,
+    strokeOpacity?: number,
+    strokeLinecap?: string,
+    strokeLinejoin?: string,
+    strokeMiterlimit?: number,
+    strokeDasharray?: string,
+    strokeDashoffset?: number,
+    vectorEffect?: string,
+    display?: string,
+    visibility?: string,
+    opacity?: number,
+    fontFamily?: string,
+    fontSize?: string,
+    fontStyle?: string,
+    fontVariant?: string,
+    fontWeight?: string,
+    fontStretch?: string,
+    letterSpacing?: string,
+    wordSpacing?: string,
+    textDecoration?: string,
+    pointerEvents?: string,
+    cursor?: string
+  ) {
     if ( fill ) {
       this.fill = fill;
+    }
+    if ( fillOpacity ) {
+      this.fillOpacity = fillOpacity;
+    }
+    if ( fillRule ) {
+      this.fillRule = fillRule;
     }
     if ( stroke ) {
       this.stroke = stroke;
@@ -42,11 +146,68 @@ export class GraphicStyle implements IStyleDeclaration {
     if ( strokeWidth ) {
       this.strokeWidth = strokeWidth;
     }
+    if ( strokeOpacity ) {
+      this.strokeOpacity = strokeOpacity;
+    }
+    if ( strokeLinecap ) {
+      this.strokeLinecap = strokeLinecap;
+    }
+    if ( strokeLinejoin ) {
+      this.strokeLinejoin = strokeLinejoin;
+    }
+    if ( strokeMiterlimit ) {
+      this.strokeMiterlimit = strokeMiterlimit;
+    }
+    if ( strokeDasharray ) {
+      this.strokeDasharray = strokeDasharray;
+    }
+    if ( strokeDashoffset ) {
+      this.strokeDashoffset = strokeDashoffset;
+    }
+    if ( vectorEffect ) {
+      this.vectorEffect = vectorEffect;
+    }
+    if ( display ) {
+      this.display = display;
+    }
+    if ( visibility ) {
+      this.visibility = visibility;
+    }
     if ( opacity ) {
       this.opacity = opacity;
     }
+    if ( fontFamily ) {
+      this.fontFamily = fontFamily;
+    }
     if ( fontSize ) {
       this.fontSize = fontSize;
+    }
+    if ( fontStyle ) {
+      this.fontStyle = fontStyle;
+    }
+    if ( fontVariant ) {
+      this.fontVariant = fontVariant;
+    }
+    if ( fontWeight ) {
+      this.fontWeight = fontWeight;
+    }
+    if ( fontStretch ) {
+      this.fontStretch = fontStretch;
+    }
+    if ( letterSpacing ) {
+      this.letterSpacing = letterSpacing;
+    }
+    if ( wordSpacing ) {
+      this.wordSpacing = wordSpacing;
+    }
+    if ( textDecoration ) {
+      this.textDecoration = textDecoration;
+    }
+    if ( pointerEvents ) {
+      this.pointerEvents = pointerEvents;
+    }
+    if ( cursor ) {
+      this.cursor = cursor;
     }
   }
 
@@ -58,14 +219,56 @@ export class GraphicStyle implements IStyleDeclaration {
   public parseAttribute( name: string, value: string ): void {
     if ( name === 'fill' ) {
       this.fill = value;
+    } else if ( name === 'fill-opacity' ) {
+      this.fillOpacity = parseFloat( value );
+    } else if ( name === 'fill-rule' ) {
+      this.fillRule = value;
     } else if ( name === 'stroke' ) {
       this.stroke = value;
     } else if ( name === 'stroke-width' ) {
       this.strokeWidth = parseFloat( value );
+    } else if ( name === 'stroke-opacity' ) {
+      this.strokeOpacity = parseFloat( value );
+    } else if ( name === 'stroke-linecap' ) {
+      this.strokeLinecap = value;
+    } else if ( name === 'stroke-linejoin' ) {
+      this.strokeLinejoin = value;
+    } else if ( name === 'stroke-miterlimit' ) {
+      this.strokeMiterlimit = parseFloat( value );
+    } else if ( name === 'stroke-dasharray' ) {
+      this.strokeDasharray = value;
+    } else if ( name === 'stroke-dashoffset' ) {
+      this.strokeDashoffset = parseFloat( value );
+    } else if ( name === 'vector-effect' ) {
+      this.vectorEffect = value;
+    } else if ( name === 'display' ) {
+      this.display = value;
+    } else if ( name === 'visibility' ) {
+      this.visibility = value;
     } else if ( name === 'opacity' ) {
       this.opacity = parseFloat( value );
+    } else if ( name === 'font-family' ) {
+      this.fontFamily = value;
     } else if ( name === 'font-size' ) {
       this.fontSize = value;
+    } else if ( name === 'font-style' ) {
+      this.fontStyle = value;
+    } else if ( name === 'font-variant' ) {
+      this.fontVariant = value;
+    } else if ( name === 'font-weight' ) {
+      this.fontWeight = value;
+    } else if ( name === 'font-stretch' ) {
+      this.fontStretch = value;
+    } else if ( name === 'letter-spacing' ) {
+      this.letterSpacing = value;
+    } else if ( name === 'word-spacing' ) {
+      this.wordSpacing = value;
+    } else if ( name === 'text-decoration' ) {
+      this.textDecoration = value;
+    } else if ( name === 'pointer-events' ) {
+      this.pointerEvents = value;
+    } else if ( name === 'cursor' ) {
+      this.cursor = value;
     }
   }
 
@@ -77,17 +280,80 @@ export class GraphicStyle implements IStyleDeclaration {
     if ( this.fill === null ) {
       this.fill = declaration.fill;
     }
+    if ( this.fillOpacity === null ) {
+      this.fillOpacity = declaration.fillOpacity;
+    }
+    if ( this.fillRule === null ) {
+      this.fillRule = declaration.fillRule;
+    }
     if ( this.stroke === null ) {
       this.stroke = declaration.stroke;
     }
     if ( this.strokeWidth === null ) {
       this.strokeWidth = declaration.strokeWidth;
     }
+    if ( this.strokeOpacity === null ) {
+      this.strokeOpacity = declaration.strokeOpacity;
+    }
+    if ( this.strokeLinecap === null ) {
+      this.strokeLinecap = declaration.strokeLinecap;
+    }
+    if ( this.strokeLinejoin === null ) {
+      this.strokeLinejoin = declaration.strokeLinejoin;
+    }
+    if ( this.strokeMiterlimit === null ) {
+      this.strokeMiterlimit = declaration.strokeMiterlimit;
+    }
+    if ( this.strokeDasharray === null ) {
+      this.strokeDasharray = declaration.strokeDasharray;
+    }
+    if ( this.strokeDashoffset === null ) {
+      this.strokeDashoffset = declaration.strokeDashoffset;
+    }
+    if ( this.vectorEffect === null ) {
+      this.vectorEffect = declaration.vectorEffect;
+    }
+    if ( this.display === null ) {
+      this.display = declaration.display;
+    }
+    if ( this.visibility === null ) {
+      this.visibility = declaration.visibility;
+    }
     if ( this.opacity === null ) {
       this.opacity = declaration.opacity;
     }
+    if ( this.fontFamily === null ) {
+      this.fontFamily = declaration.fontFamily;
+    }
     if ( this.fontSize === null ) {
       this.fontSize = declaration.fontSize;
+    }
+    if ( this.fontStyle === null ) {
+      this.fontStyle = declaration.fontStyle;
+    }
+    if ( this.fontVariant === null ) {
+      this.fontVariant = declaration.fontVariant;
+    }
+    if ( this.fontWeight === null ) {
+      this.fontWeight = declaration.fontWeight;
+    }
+    if ( this.fontStretch === null ) {
+      this.fontStretch = declaration.fontStretch;
+    }
+    if ( this.letterSpacing === null ) {
+      this.letterSpacing = declaration.letterSpacing;
+    }
+    if ( this.wordSpacing === null ) {
+      this.wordSpacing = declaration.wordSpacing;
+    }
+    if ( this.textDecoration === null ) {
+      this.textDecoration = declaration.textDecoration;
+    }
+    if ( this.pointerEvents === null ) {
+      this.pointerEvents = declaration.pointerEvents;
+    }
+    if ( this.cursor === null ) {
+      this.cursor = declaration.cursor;
     }
   }
 }
--- test/graphicStyle.spec.ts
+++ test/graphicStyle.spec.ts
@@ -30,14 +30,37 @@ describe( 'GraphicStyle', (): void => {
   describe( 'constructor', (): void => {
     it( 'should set the properties', (): void => {
       // Act
-      const style = new GraphicStyle( 'black', 'blue', 2, 1, '12px' );
+      const style = new GraphicStyle( 'black', 0.9, 'evenodd', 'blue', 2, 0.8, 'square', 'bevel', 4,
+        '1,2,3', 10, 'non-scaling-stroke', 'block', 'hidden', 0.7, 'sans-serif', '12px', 'italic',
+        'small-caps', 'bold', 'ultra-condensed', '2px', '3px', 'line-through', 'fill', 'pointer' );
 
       // Assert
       expect( style.fill ).to.equal( 'black' );
+      expect( style.fillOpacity ).to.equal( 0.9 );
+      expect( style.fillRule ).to.equal( 'evenodd' );
       expect( style.stroke ).to.equal( 'blue' );
       expect( style.strokeWidth ).to.equal( 2 );
-      expect( style.opacity ).to.equal( 1 );
+      expect( style.strokeOpacity ).to.equal( 0.8 );
+      expect( style.strokeLinecap ).to.equal( 'square' );
+      expect( style.strokeLinejoin ).to.equal( 'bevel' );
+      expect( style.strokeMiterlimit ).to.equal( 4 );
+      expect( style.strokeDasharray ).to.equal( '1,2,3' );
+      expect( style.strokeDashoffset ).to.equal( 10 );
+      expect( style.vectorEffect ).to.equal( 'non-scaling-stroke' );
+      expect( style.display ).to.equal( 'block' );
+      expect( style.visibility ).to.equal( 'hidden' );
+      expect( style.opacity ).to.equal( 0.7 );
+      expect( style.fontFamily ).to.equal( 'sans-serif' );
       expect( style.fontSize ).to.equal( '12px' );
+      expect( style.fontStyle ).to.equal( 'italic' );
+      expect( style.fontVariant ).to.equal( 'small-caps' );
+      expect( style.fontWeight ).to.equal( 'bold' );
+      expect( style.fontStretch ).to.equal( 'ultra-condensed' );
+      expect( style.letterSpacing ).to.equal( '2px' );
+      expect( style.wordSpacing ).to.equal( '3px' );
+      expect( style.textDecoration ).to.equal( 'line-through' );
+      expect( style.pointerEvents ).to.equal( 'fill' );
+      expect( style.cursor ).to.equal( 'pointer' );
     } );
 
     it( 'should create empty style when no parameters are passed', (): void => {
@@ -56,13 +79,48 @@ describe( 'GraphicStyle', (): void => {
 
       // Act
       style.parseAttribute( 'fill', 'black' );
+      style.parseAttribute( 'fill-rule', 'evenodd' );
       style.parseAttribute( 'stroke', 'blue' );
+      style.parseAttribute( 'stroke-linecap', 'square' );
+      style.parseAttribute( 'stroke-linejoin', 'bevel' );
+      style.parseAttribute( 'stroke-dasharray', '1,2,3' );
+      style.parseAttribute( 'vector-effect', 'non-scaling-stroke' );
+      style.parseAttribute( 'display', 'block' );
+      style.parseAttribute( 'visibility', 'hidden' );
+      style.parseAttribute( 'font-family', 'sans-serif' );
       style.parseAttribute( 'font-size', '12px' );
+      style.parseAttribute( 'font-style', 'italic' );
+      style.parseAttribute( 'font-variant', 'small-caps' );
+      style.parseAttribute( 'font-weight', 'bold' );
+      style.parseAttribute( 'font-stretch', 'ultra-condensed' );
+      style.parseAttribute( 'letter-spacing', '2px' );
+      style.parseAttribute( 'word-spacing', '3px' );
+      style.parseAttribute( 'text-decoration', 'line-through' );
+      style.parseAttribute( 'pointer-events', 'fill' );
+      style.parseAttribute( 'cursor', 'pointer' );
+      style.parseAttribute( 'to-be-ignored', 'undefined' );
 
       // Assert
       expect( style.fill ).to.equal( 'black' );
+      expect( style.fillRule ).to.equal( 'evenodd' );
       expect( style.stroke ).to.equal( 'blue' );
+      expect( style.strokeLinecap ).to.equal( 'square' );
+      expect( style.strokeLinejoin ).to.equal( 'bevel' );
+      expect( style.strokeDasharray ).to.equal( '1,2,3' );
+      expect( style.vectorEffect ).to.equal( 'non-scaling-stroke' );
+      expect( style.display ).to.equal( 'block' );
+      expect( style.visibility ).to.equal( 'hidden' );
+      expect( style.fontFamily ).to.equal( 'sans-serif' );
       expect( style.fontSize ).to.equal( '12px' );
+      expect( style.fontStyle ).to.equal( 'italic' );
+      expect( style.fontVariant ).to.equal( 'small-caps' );
+      expect( style.fontWeight ).to.equal( 'bold' );
+      expect( style.fontStretch ).to.equal( 'ultra-condensed' );
+      expect( style.letterSpacing ).to.equal( '2px' );
+      expect( style.wordSpacing ).to.equal( '3px' );
+      expect( style.textDecoration ).to.equal( 'line-through' );
+      expect( style.pointerEvents ).to.equal( 'fill' );
+      expect( style.cursor ).to.equal( 'pointer' );
     } );
 
     it( 'should parse float value properties', (): void => {
@@ -70,36 +128,71 @@ describe( 'GraphicStyle', (): void => {
       const style = new GraphicStyle();
 
       // Act
-      style.parseAttribute( 'stroke-width', '1.2' );
-      style.parseAttribute( 'opacity', '0.5' );
+      style.parseAttribute( 'fill-opacity', '0.9' );
+      style.parseAttribute( 'stroke-width', '2' );
+      style.parseAttribute( 'stroke-opacity', '0.8' );
+      style.parseAttribute( 'stroke-miterlimit', '4' );
+      style.parseAttribute( 'stroke-dashoffset', '10' );
+      style.parseAttribute( 'opacity', '0.7' );
 
       // Assert
-      expect( style.strokeWidth ).to.equal( 1.2 );
-      expect( style.opacity ).to.equal( 0.5 );
+      expect( style.fillOpacity ).to.equal( 0.9 );
+      expect( style.strokeWidth ).to.equal( 2 );
+      expect( style.strokeOpacity ).to.equal( 0.8 );
+      expect( style.strokeMiterlimit ).to.equal( 4 );
+      expect( style.strokeDashoffset ).to.equal( 10 );
+      expect( style.opacity ).to.equal( 0.7 );
     } );
   } );
 
   describe( 'complementWith', (): void => {
     it( 'should copy the values from the source style into an empty target style', (): void => {
       // Arrange
-      const targetStyle = new GraphicStyle();
-      const sourceStyle = new GraphicStyle( 'black', 'blue', 2, 1, '12px' );
+      const style = new GraphicStyle();
+      const sourceStyle = new GraphicStyle( 'black', 0.9, 'evenodd', 'blue', 2, 0.8, 'square',
+        'bevel', 4, '1,2,3', 10, 'non-scaling-stroke', 'block', 'hidden', 0.7, 'sans-serif', '12px',
+        'italic', 'small-caps', 'bold', 'ultra-condensed', '2px', '3px', 'line-through', 'fill',
+        'pointer' );
 
       // Act
-      targetStyle.complementWith( sourceStyle );
+      style.complementWith( sourceStyle );
 
       // Assert
-      expect( targetStyle.fill ).to.equal( 'black' );
-      expect( targetStyle.stroke ).to.equal( 'blue' );
-      expect( targetStyle.strokeWidth ).to.equal( 2 );
-      expect( targetStyle.opacity ).to.equal( 1 );
-      expect( targetStyle.fontSize ).to.equal( '12px' );
+      expect( style.fill ).to.equal( 'black' );
+      expect( style.fillOpacity ).to.equal( 0.9 );
+      expect( style.fillRule ).to.equal( 'evenodd' );
+      expect( style.stroke ).to.equal( 'blue' );
+      expect( style.strokeWidth ).to.equal( 2 );
+      expect( style.strokeOpacity ).to.equal( 0.8 );
+      expect( style.strokeLinecap ).to.equal( 'square' );
+      expect( style.strokeLinejoin ).to.equal( 'bevel' );
+      expect( style.strokeMiterlimit ).to.equal( 4 );
+      expect( style.strokeDasharray ).to.equal( '1,2,3' );
+      expect( style.strokeDashoffset ).to.equal( 10 );
+      expect( style.vectorEffect ).to.equal( 'non-scaling-stroke' );
+      expect( style.display ).to.equal( 'block' );
+      expect( style.visibility ).to.equal( 'hidden' );
+      expect( style.opacity ).to.equal( 0.7 );
+      expect( style.fontFamily ).to.equal( 'sans-serif' );
+      expect( style.fontSize ).to.equal( '12px' );
+      expect( style.fontStyle ).to.equal( 'italic' );
+      expect( style.fontVariant ).to.equal( 'small-caps' );
+      expect( style.fontWeight ).to.equal( 'bold' );
+      expect( style.fontStretch ).to.equal( 'ultra-condensed' );
+      expect( style.letterSpacing ).to.equal( '2px' );
+      expect( style.wordSpacing ).to.equal( '3px' );
+      expect( style.textDecoration ).to.equal( 'line-through' );
+      expect( style.pointerEvents ).to.equal( 'fill' );
+      expect( style.cursor ).to.equal( 'pointer' );
     } );
 
     it( 'should not change values in target style if they have already been set', (): void => {
       // Arrange
-      const targetStyle = new GraphicStyle( 'red', 'yellow', 6, 7, '24px' );
-      const sourceStyle = new GraphicStyle( 'black', 'blue', 2, 1, '12px' );
+      const targetStyle = new GraphicStyle( 'red', 0.9, 'evenodd', 'yellow', 6, 0.8, 'square',
+        'bevel', 4, '1,2,3', 10, 'non-scaling-stroke', 'block', 'hidden', 0.7, 'sans-serif', '12px',
+        'italic', 'small-caps', 'bold', 'ultra-condensed', '2px', '3px', 'line-through', 'fill',
+        'pointer' );
+      const sourceStyle = new GraphicStyle( 'black', undefined, undefined, 'blue', 2 );
 
       // Act
       targetStyle.complementWith( sourceStyle );
@@ -108,24 +201,21 @@ describe( 'GraphicStyle', (): void => {
       expect( targetStyle.fill ).to.equal( 'red' );
       expect( targetStyle.stroke ).to.equal( 'yellow' );
       expect( targetStyle.strokeWidth ).to.equal( 6 );
-      expect( targetStyle.opacity ).to.equal( 7 );
-      expect( targetStyle.fontSize ).to.equal( '24px' );
     } );
 
     it( 'should copy those values that have not been set already', (): void => {
       // Arrange
-      const targetStyle = new GraphicStyle( 'red', undefined, 6, undefined, '24px' );
-      const sourceStyle = new GraphicStyle( 'black', 'blue', 2, 1, '12px' );
+      const targetStyle = new GraphicStyle( 'red', undefined, undefined, undefined, 6 );
+      const sourceStyle = new GraphicStyle( 'black', 0.5, undefined, 'blue', 2 );
 
       // Act
       targetStyle.complementWith( sourceStyle );
 
       // Assert
       expect( targetStyle.fill ).to.equal( 'red' );
+      expect( targetStyle.fillOpacity ).to.equal( 0.5 );
       expect( targetStyle.stroke ).to.equal( 'blue' );
       expect( targetStyle.strokeWidth ).to.equal( 6 );
-      expect( targetStyle.opacity ).to.equal( 1 );
-      expect( targetStyle.fontSize ).to.equal( '24px' );
     } );
   } );
 } );
